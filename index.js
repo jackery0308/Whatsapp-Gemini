0c081edf896fd6146d5ab7f7e1cca61b6a9c18db
Pang <jackery0308@hotmail.com>
2023-12-19 14:02:22 +0800

    add error handling

--- index.js
+++ index.js
@@ -19,19 +19,41 @@ client.initialize();
 const genAI = new GoogleGenerativeAI(process.env.Gemini_API_KEY);
 
 client.on('message', message => {
-    console.log(message.from.toString() + ":");
+  console.log(message.from.toString() + ":");
   console.log(message.body);
-  runCompletion(message.body.substring(1)).then(result => message.reply(result));
- 
+
+  runCompletion(message.body.substring(1)).then(result => message.reply(result));  
 });
  
 async function runCompletion (message) {
   // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-  const result = await model.generateContent(message);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-  return text;
+  try {
+    const result = await model.generateContent(message);
+    const response = await result.response;
+
+    // Check for successful generation
+    if (response.promptFeedback.blockReason == null) {
+      const text = response.text();
+      console.log(text);
+      return text;
+    } else {
+      // Handle specific error types
+      if (response.promptFeedback.blockReason === "SAFETY") {
+        console.error("Text blocked due to safety concerns. Please rephrase your prompt.");
+        return "Your request generated content that violates safety guidelines. Please try again with a different prompt.";
+      } else if (response.promptFeedback.blockReason === "MODEL_UNAVAILABLE") {
+        console.error("Model unavailable. Please try again later.");
+        return "The model is currently unavailable. Please try again later.";
+      } else {
+        // General error handling
+        console.error("Generation failed", response.error);
+        return `An error occurred while generating content: ${response.error.message}`;
+      }
+    }
+  } catch (Ex) {
+    console.error(Ex);
+    return `Unexpected error: ${Ex.message}`;
+  }
 }
